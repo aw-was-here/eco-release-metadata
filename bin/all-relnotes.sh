96ee32a1275b5be4d313ff356b73585a0a3378fd
Allen Wittenauer <aw@apache.org>
2015-07-05 20:38:50 -0700

    reorder

--- bin/all-relnotes.sh
+++ bin/all-relnotes.sh
@@ -11,29 +11,13 @@ releasedocmaker.py --project HADOOP --project HDFS --project YARN --project MAPR
         --projecttitle "Apache Hadoop" \
         --range --version 2.6.0 --version 3.0.0
 
-cd ${HOME}/Src/aw-github/eco-release-metadata/PIG
-releasedocmaker.py --project PIG --projecttitle "Apache Pig" \
-	--range --version 0.15.0 --version 0.17.0
-
-cd ${HOME}/Src/aw-github/eco-release-metadata/ZOOKEEPER
-releasedocmaker.py --project ZOOKEEPER --projecttitle "Apache Zookeeper" \
-        --range --version 3.5.0 --version 4.0.0
-
 cd ${HOME}/Src/aw-github/eco-release-metadata/HBASE
 releasedocmaker.py --project HBASE --projecttitle "Apache HBase" \
-        --range --version 1.0.0 --version 2.0.0
+        --range --version 1.0.3 --version 2.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/HIVE
 releasedocmaker.py --project HIVE --projecttitle "Apache Hive" \
         --range --version 1.1.0 --version 2.0.0
-
-cd ${HOME}/Src/aw-github/eco-release-metadata/SPARK
-releasedocmaker.py --project SPARK --projecttitle "Apache Spark" \
-        --range --version 1.2.0 --version 2+
-
-cd ${HOME}/Src/aw-github/eco-release-metadata/TEZ
-releasedocmaker.py --project TEZ --projecttitle "Apache Tez" \
-        --range --version 0.6.0 --version 1.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/KAFKA
 releasedocmaker.py --project KAFKA --projecttitle "Apache Kafka" \
@@ -41,11 +25,28 @@ releasedocmaker.py --project KAFKA --projecttitle "Apache Kafka" \
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/OOZIE
 releasedocmaker.py --project OOZIE --projecttitle "Apache Oozie" \
-        --range --version 4.1.0 --version 4.4.0
+        --range --version 4.2.0 --version 5.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/ORC
 releasedocmaker.py --project ORC --projecttitle "Apache Orc" \
         --range --version 1.0.0 --version 2.0.0
+
+cd ${HOME}/Src/aw-github/eco-release-metadata/PIG
+releasedocmaker.py --project PIG --projecttitle "Apache Pig" \
+        --range --version 0.15.0 --version 0.17.0
+
+cd ${HOME}/Src/aw-github/eco-release-metadata/SPARK
+releasedocmaker.py --project SPARK --projecttitle "Apache Spark" \
+        --range --version 1.2.0 --version 2+
+
+cd ${HOME}/Src/aw-github/eco-release-metadata/TEZ
+releasedocmaker.py --project TEZ --projecttitle "Apache Tez" \
+        --range --version 0.5.0 --version 1.0.0
+
+cd ${HOME}/Src/aw-github/eco-release-metadata/ZOOKEEPER
+releasedocmaker.py --project ZOOKEEPER --projecttitle "Apache Zookeeper" \
+        --range --version 3.5.0 --version 4.0.0
+
 
 d=$(date -u "+%Y-%m-%d %H:%m")
 git commit -a -m "${d} UTC auto-commit"
