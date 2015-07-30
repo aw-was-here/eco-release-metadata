72ca1c55821f3da8b971d8b5a14ebfda80ae7ce8
Allen Wittenauer <aw@apache.org>
2015-07-29 18:32:45 -0700

    Switch to yetus for releasedocmaker

--- bin/all-relnotes.sh
+++ bin/all-relnotes.sh
@@ -3,61 +3,61 @@
 this="${BASH_SOURCE-$0}"
 bin=$(cd -P -- "$(dirname -- "${this}")" >/dev/null && pwd -P)
 
-OPATH=${PATH}
-PATH=${bin}:${OPATH}
+#
+# This is a good test for Yetus...
+#
+RDM=${HOME}/Src/apache-git/hadoop-yetus/dev-support/releasedocmaker.py
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/YETUS
-releasedocmaker.py --project HADOOP \
+${RDM} --project HADOOP \
         --projecttitle "Apache Hadoop Yetus" \
         --version HADOOP-12111
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/BIGTOP
-releasedocmaker.py --project BIGTOP --projecttitle "Apache BigTop" \
+${RDM} --project BIGTOP --projecttitle "Apache BigTop" \
         --range --version 1.0.0 --version 2.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/HADOOP
-releasedocmaker.py --project HADOOP --project HDFS --project YARN --project MAPREDUCE \
+${RDM} --project HADOOP --project HDFS --project YARN --project MAPREDUCE \
         --projecttitle "Apache Hadoop" \
         --range --version 2.6.0 --version 3.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/HBASE
-releasedocmaker.py --project HBASE --projecttitle "Apache HBase" \
+${RDM} --project HBASE --projecttitle "Apache HBase" \
         --range --version 1.0.3 --version 2.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/HIVE
-releasedocmaker.py --project HIVE --projecttitle "Apache Hive" \
+${RDM} --project HIVE --projecttitle "Apache Hive" \
         --range --version 1.1.0 --version 2.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/KAFKA
-releasedocmaker.py --project KAFKA --projecttitle "Apache Kafka" \
+${RDM} --project KAFKA --projecttitle "Apache Kafka" \
         --range --version 0.8.1 --version 1.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/OOZIE
-releasedocmaker.py --project OOZIE --projecttitle "Apache Oozie" \
+${RDM} --project OOZIE --projecttitle "Apache Oozie" \
         --range --version 4.2.0 --version 5.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/ORC
-releasedocmaker.py --project ORC --projecttitle "Apache Orc" \
+${RDM} --project ORC --projecttitle "Apache Orc" \
         --range --version 1.0.0 --version 2.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/PIG
-releasedocmaker.py --project PIG --projecttitle "Apache Pig" \
+${RDM} --project PIG --projecttitle "Apache Pig" \
         --range --version 0.15.0 --version 0.17.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/SPARK
-releasedocmaker.py --project SPARK --projecttitle "Apache Spark" \
+${RDM} --project SPARK --projecttitle "Apache Spark" \
         --range --version 1.2.0 --version 2+
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/TEZ
-releasedocmaker.py --project TEZ --projecttitle "Apache Tez" \
+${RDM} --project TEZ --projecttitle "Apache Tez" \
         --range --version 0.5.0 --version 1.0.0
 
 cd ${HOME}/Src/aw-github/eco-release-metadata/ZOOKEEPER
-releasedocmaker.py --project ZOOKEEPER --projecttitle "Apache Zookeeper" \
+${RDM} --project ZOOKEEPER --projecttitle "Apache Zookeeper" \
         --range --version 3.5.0 --version 4.0.0
 
 
 d=$(date -u "+%Y-%m-%d %H:%m")
 git commit -a -m "${d} UTC auto-commit"
-
-PATH=${OPATH}
