98492c3b51c06f5a594e2252d1c4b0592a625546
Allen Wittenauer <aw@apache.org>
2018-01-25 08:21:36 -0800

    more projects

--- bin/all-relnotes.sh
+++ bin/all-relnotes.sh
@@ -60,13 +60,17 @@ bin=$(cd -P -- "$(dirname -- "${this}")" >/dev/null && pwd -P)
 #
 RDM=${HOME}/shared-vmware/yetus/release-doc-maker/releasedocmaker.py
 
-common_rdm yetus 0.1.0 1.0.0
+common_rdm yetus 0.7.0 1.0.0
 
-common_rdm bigtop 1.0.1 2.0.0 BigTop
+common_rdm bigtop 1.3.0 2.0.0 BigTop
 
 common_rdm falcon 0.10 1.0
 
-common_rdm flink 1.1.5 2.0.0
+common_rdm flink 1.4.0 2.0.0
+
+common_rdm kudu 1.0.0 2.0.0
+
+common_rdm impala 2.0.0 3.0.0
 
 common_rdm mahout 0.13.0 2.0.0
 
@@ -78,19 +82,23 @@ common_rdm parquet 1.5.0 2.0.0
 
 common_rdm pig 0.15.1 1.0.0
 
-common_rdm zookeeper 3.5.2 4.0.0
+common_rdm ranger 0.7.2 1.0.0
+
+common_rdm sentry 1.0.0 3.0.0
+
+common_rdm zookeeper 3.5.4 4.0.0
 
 # Here are the problematic ones:
 
-common_rdm hbase 1.1.11 1.1.99 HBase
-common_rdm hbase 1.2.7 1.2.99 HBase
-common_rdm hbase 1.3.2 1.3.99 HBase
-common_rdm hbase 1.4.1 2.0.0 HBase
+#common_rdm hbase 1.1.11 1.1.99 HBase
+common_rdm hbase 1.2.8 1.2.99 HBase
+common_rdm hbase 1.3.3 1.3.99 HBase
+common_rdm hbase 1.4.2 2.0.0 HBase
 
-common_rdm hive 1.1.2 1.4.0
+#common_rdm hive 1.1.2 1.4.0
 common_rdm hive 2.1.2 3.0.0
 
-common_rdm kafka 0.10.1.2 1.0.0
+common_rdm kafka 0.10.1.2 1.1.0
 
 common_rdm spark 2.0.0 3.0.0
 
@@ -110,21 +118,28 @@ print_and_run \
 print_and_run \
 	"${PYTHON}" "${RDM}" --project HADOOP --project HDFS --project YARN --project MAPREDUCE \
         --projecttitle "Apache Hadoop" \
-        --range --version 2.7.4 --version 2.7.9 \
+        --range --version 2.7.6 --version 2.7.9 \
         --index --license \
         --outputdir HADOOP
 
 print_and_run \
         "${PYTHON}" "${RDM}" --project HADOOP --project HDFS --project YARN --project MAPREDUCE \
         --projecttitle "Apache Hadoop" \
-        --range --version 2.8.1 --version 2.10.0 \
+        --range --version 2.8.4 --version 2.8.9 \
+        --index --license \
+        --outputdir HADOOP
+
+print_and_run \
+	"${PYTHON}" "${RDM}" --project HADOOP --project HDFS --project YARN --project MAPREDUCE \
+        --projecttitle "Apache Hadoop" \
+        --range --version 2.9.1 --version 2.9.9 \
         --index --license \
         --outputdir HADOOP
 
 print_and_run \
         "${PYTHON}" "${RDM}" --project HADOOP --project HDFS --project YARN --project MAPREDUCE \
         --projecttitle "Apache Hadoop" \
-        --range --version 3.0.0 --version 4 \
+        --range --version 3.0.1 --version 4 \
         --index --license \
         --outputdir HADOOP
 
